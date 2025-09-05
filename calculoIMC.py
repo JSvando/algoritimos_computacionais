fa41dd50618ca36b71b34d73809346404328d02d
unknown <josevandersondelimaoliveira@gmail.com>
2025-09-05 00:17:30 -0300

    calculo IMC e python

---
+++ calculoIMC.py
@@ -0,0 +1,10 @@
+nome = "jose vanderson"
+idade = 21
+peso = 54.0
+altura = 164
+imc = peso / (altura * altura)
+print (nome)
+print ("sua idade é" , idade)
+print ("seu peso é", peso, "kg")
+print ("sua altura é", altura, "cm")
+print ("seu imc é", imc)
