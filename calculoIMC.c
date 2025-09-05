558cc2e7d6a3953b7700abf7446799a39e5f7b22
JSvando <josevandersondelimaoliveira@gmail.com>
2025-09-05 02:49:45 -0300

    calculoIMC.c

--- calculoIMC.c
+++ calculoIMC.c
@@ -1,34 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    double peso, altura, imc;
+    char nome[] = "jose vanderson";
+    int idade = 21;
+    double peso = 54.0;
+    double altura = 164; // em cm
+    double imc = peso / (altura * altura);
 
-    // Entrada
-    printf("Digite seu peso (kg): ");
-    scanf("%lf", &peso);
-
-    printf("Digite sua altura (m): ");
-    scanf("%lf", &altura);
-
-    // Cálculo
-    imc = peso / (altura * altura);
-
-    // Classificação
-    printf("IMC: %.2f - ", imc);
-
-    if (imc < 18.5) {
-        printf("Abaixo do peso\n");
-    } else if (imc < 24.9) {
-        printf("Peso normal\n");
-    } else if (imc < 29.9) {
-        printf("Sobrepeso\n");
-    } else if (imc < 34.9) {
-        printf("Obesidade grau I\n");
-    } else if (imc < 39.9) {
-        printf("Obesidade grau II\n");
-    } else {
-        printf("Obesidade grau III\n");
-    }
+    printf("%s\n", nome);
+    printf("sua idade é %d\n", idade);
+    printf("seu peso é %.1f kg\n", peso);
+    printf("sua altura é %.0f cm\n", altura);
+    printf("seu imc é %f\n", imc);
 
     return 0;
 }
