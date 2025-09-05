221c61d3ba7c130912777a6cf20f25a5f20a10ca
JSvando <josevandersondelimaoliveira@gmail.com>
2025-09-05 02:53:01 -0300

    calculoIMC.php

---
+++ calculoIMC.php
@@ -0,0 +1,13 @@
+<?php
+$nome = "jose vanderson";
+$idade = 21;
+$peso = 54.0;
+$altura = 164; // em cm
+$imc = $peso / ($altura * $altura);
+
+echo $nome . "\n";
+echo "sua idade é " . $idade . "\n";
+echo "seu peso é " . $peso . " kg\n";
+echo "sua altura é " . $altura . " cm\n";
+echo "seu imc é " . $imc . "\n";
+?>
